f3b92e8d77ab45b147b9f6bea5215a36b275b5d1
George Hokkanen <hokkanen@stanford.edu>
2011-11-04 17:20:53 -0700

    Self-similarity matrix.m and SIPLCA lib included

--- matlab/selfSimilarity.m
+++ matlab/selfSimilarity.m
@@ -1,1 +1,12 @@
 %Self-similarity matrix test
+song        = mp3read('files/RoboBooty.mp3');
+200
+song        = song(:,1);
+buffersize  = 2^10*2^4;
+transform   = vec2mat(song,buffersize);
+transform   = real(fft(transform));
+n           = sqrt(sum(transform.^2,2));
+n           = n(:,ones(1,size(transform,2)));
+normalized  = transform./n;
+simMat      = normalized*normalized';%/(buffersize^2);
+simMat      = fliplr(simMat)';
